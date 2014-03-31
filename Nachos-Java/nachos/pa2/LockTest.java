0685e5675b835d8afa2f5422d3a63cb808a41888
Shaman16x <shaman16x@gmail.com>
2014-03-31 18:47:41 -0500

    Made LockTest work appropriatly

--- Nachos-Java/nachos/pa2/LockTest.java
+++ Nachos-Java/nachos/pa2/LockTest.java
@@ -2,24 +2,33 @@ package nachos.pa2;
 
 import nachos.threads.*;
 
-// waits for x number of miliseconds
-// then finishes execution
-public class DelayTest implements Runnable {
-    public DelayTest(int num, int time) {
+// Testing program for pa2
+// When executed this program:
+// gets a lock,
+// waits x miliseconds
+// and releases the lock
+
+public class LockTest implements Runnable {
+    public LockTest(String name, int time, Lock lock) {
         this.time = time;
-        this.num = num;
+        this.name = name;
+        this.lock = lock;
     }
 
+    // Aqu
     public void run() {
+        lock.acquire();
         for (int i=0; i<time; i++) {
             waitMS();
             
             if(i%5 == 0)   {
-                System.out.println("*** thread " + num + " waited "
+                System.out.println("*** " + name + " waited "
                        + i + " ms");
             }
             KThread.currentThread().yield();
         }
+        System.out.println("*** " + name + " has finished");
+        lock.release();
     }
 
     // wait for 1ms
@@ -29,7 +38,8 @@ public class DelayTest implements Runnable {
         while((System.nanoTime() - start) < 1000000);
     }
     
-    private int num;
+    private String name;
     private int count = 0;
     private int time;
+    private Lock lock;
 }
