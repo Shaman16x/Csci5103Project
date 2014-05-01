ae276ef42bf4afa6914195ba20200a2f81f6f58e
jery777 <thom3839@umn.edu>
2014-05-01 17:31:25 -0500

    almost done

--- Nachos-Java/nachos/userprog/UserKernel.java
+++ Nachos-Java/nachos/userprog/UserKernel.java
@@ -34,7 +34,7 @@ public class UserKernel extends ThreadedKernel {
      * Test the console device.
      */	
     public void selfTest() {
-	super.selfTest();
+	/*super.selfTest();
 
 	System.out.println("Testing the console device. Typed characters");
 	System.out.println("will be echoed until q is typed.");
@@ -47,7 +47,7 @@ public class UserKernel extends ThreadedKernel {
 	}
 	while (c != 'q');
 
-	System.out.println("");
+	System.out.println("");*/
     }
 
     /**
