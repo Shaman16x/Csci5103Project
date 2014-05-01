1dfe5c99b6e8ebeaf556565f8e82f47b5f151802
jery777 <thom3839@umn.edu>
2014-05-01 17:24:57 -0500

    finished part 6

--- Nachos-Java/nachos/userprog/UserProcess.java
+++ Nachos-Java/nachos/userprog/UserProcess.java
@@ -429,8 +429,8 @@ public class UserProcess {
     private int handleHalt() {
         System.out.println("\n\nPA3 Statistics");
         System.out.println("Maximum Processes:     " + maxProcesses);
-        System.out.println("Maximum Reserved Pages ");
-        System.out.println("Maximum Mapped Frames: ");
+        System.out.println("Maximum Reserved Pages " + allocator.getMaxNumReserved());
+        System.out.println("Maximum Mapped Frames: " + allocator.getMaxNumMapped());
         
         Machine.halt();
         
